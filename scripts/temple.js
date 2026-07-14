e55dca5c044c4dc393ed45478906f69e6474865d
robbinsonlukuru <rlukuru@byupathway.edu>
2026-07-14 07:58:25 +0300

    temple

---
+++ scripts/temple.js
@@ -0,0 +1,17 @@
+const modified = document.querySelector("#lastModified");
+
+modified.textContent = `Last Modified: ${document.lastModified}`;
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        menuButton.textContent = "✖";
+    } else {
+        menuButton.textContent = "☰";
+    }
+
+});
