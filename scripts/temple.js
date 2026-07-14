f3ab6cc3ec8f4a6c4f27c8a7295df8ceead0afa9
robbinsonlukuru <rlukuru@byupathway.edu>
2026-07-14 08:14:50 +0300

    js

--- scripts/temple.js
+++ scripts/temple.js
@@ -15,3 +15,15 @@ menuButton.addEventListener("click", () => {
     }
 
 });
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+});
+
+document.querySelector("#currentyear").textContent =
+    new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
